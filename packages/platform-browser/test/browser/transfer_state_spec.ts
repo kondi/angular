74b31005009aaf5ccb5173ec3be1600caa6124c6
Pusztai Tibor <kondi.elte@gmail.com>
2018-03-27 14:20:39 +0200

    fix(platform-browser): typesafe transfer state key
    Make `StateKey` typesafe by reefing the type.

--- packages/platform-browser/test/browser/transfer_state_spec.ts
+++ packages/platform-browser/test/browser/transfer_state_spec.ts
@@ -37,6 +37,7 @@ describe('TransferState', () => {
   let doc: Document;
 
   const TEST_KEY = makeStateKey<number>('test');
+  const BOOLEAN_KEY = makeStateKey<boolean>('boolean');
   const DELAYED_KEY = makeStateKey<string>('delayed');
 
   beforeEach(() => {
@@ -80,9 +81,9 @@ describe('TransferState', () => {
 
   it('supports setting and accessing value \'false\' via get', () => {
     const transferState: TransferState = TestBed.inject(TransferState);
-    transferState.set(TEST_KEY, false);
-    expect(transferState.get(TEST_KEY, true)).toBe(false);
-    expect(transferState.hasKey(TEST_KEY)).toBe(true);
+    transferState.set(BOOLEAN_KEY, false);
+    expect(transferState.get(BOOLEAN_KEY, true)).toBe(false);
+    expect(transferState.hasKey(BOOLEAN_KEY)).toBe(true);
   });
 
   it('supports setting and accessing value \'null\' via get', () => {
